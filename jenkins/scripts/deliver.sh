87edcb587f885f7b80786e0e0dc8a4e8ee1ea679
tioramadhn <tioramadhntio@aapps.ipb.ac.id>
2023-06-27 16:57:39 +0700

    fix wait 1m before apps closed

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -26,11 +26,5 @@ set -x
 java -jar target/${NAME}-${VERSION}.jar &
 sleep 1
 echo $! > .pidfile
+echo $!
 set +x
-echo 'Jeda 1 Menit sebelum aplikasi ditutup'
-sleep 1m
-echo 'The following command terminates the "npm start" process using its PID'
-echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
-echo 'was executed.'
-set -x
-kill $(cat .pidfile)
