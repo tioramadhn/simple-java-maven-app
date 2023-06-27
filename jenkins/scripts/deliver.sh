50d9eb00773d4c6b6f4815baf0a681568e724d72
tioramadhn <tioramadhntio@aapps.ipb.ac.id>
2023-06-27 21:49:24 +0700

    fix wait 1m before apps closed

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,12 +23,4 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 java -jar target/${NAME}-${VERSION}.jar &
-sleep 1
-echo $! > .pidfile
-echo $!
-set +x
-echo 'Jeda 1 Menit sebelum aplikasi ditutup'
-sleep 1m
-set -x
-kill $(cat .pidfile)
 
