81fa5ff1afd99ac1d6bb3c0d531f6e2b3310226f
tioramadhn <tioramadhntio@aapps.ipb.ac.id>
2023-06-27 16:51:54 +0700

    fix wait 1m before apps closed

--- jenkins/scripts/kill.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-echo 'Jeda 1 Menit sebelum aplikasi ditutup'
-sleep 1m
-echo 'The following command terminates the "npm start" process using its PID'
-echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
-echo 'was executed.'
-set -x
-kill $(cat .pidfile)
